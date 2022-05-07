3675cf412eea464327b695b337bc807e946a9cf4
amirb <amirbou@gmail.com>
2022-05-07 03:19:02 +0300

    replaced clap builder API with declerative API and added an option to test a custom count and offset to read from

--- src/bin/tester.rs
+++ src/bin/tester.rs
@@ -1,6 +1,6 @@
-use clap::{arg, Command, ArgMatches};
+use clap::{Parser, Subcommand};
 use libc::{ssize_t, size_t, c_int};
-use std::{fs::File, ffi::CString, os::unix::prelude::AsRawFd};
+use std::{fs::File, ffi::{CString, OsStr, OsString}, os::unix::prelude::AsRawFd, io::{Seek, SeekFrom}};
 #[macro_use]
 extern crate dlopen_derive;
 extern crate dlopen;
@@ -12,37 +12,80 @@ struct IOLibrary {
     write_all: extern "C" fn(fd: c_int, data: *const u8, size: size_t) -> ssize_t
 }
 
-fn load_library(library_path: String) -> Container<IOLibrary>
+#[derive(Parser)]
+#[clap(author, version, long_about = None)]
+#[clap(about = "Tests a given r/w wrapper library")]
+struct TesterCli {
+    
+    /// The path to the library
+    library_path: OsString,
+    
+    /// The path to the file to read
+    file_path: OsString,
+
+    #[clap(subcommand)]
+    command: Commands
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Tests the read_all functionality of the library
+    Read { 
+        #[clap(long)]
+        #[clap(default_value_t = 0)]
+        /// The offset to start reading from
+        offset: u64,
+        #[clap(long)]
+        /// The count to read. by default, the whole file is read
+        count: Option<usize>,
+    },
+
+    /// Tests the write_all functionality of the library
+    Write {
+        /// The data to write
+        data: String,
+    }
+}
+
+
+fn load_library(library_path: &OsStr) -> Container<IOLibrary>
 {
 
     unsafe { Container::load(library_path).expect("Failed to load the library!") }
 
 }
 
-fn open_file(file_path: String, for_write: bool) -> File {
+fn open_file(file_path: &OsStr, for_write: bool) -> File {
     File::options().read(true).write(for_write).open(file_path).expect("Failed to open file!")
 }
 
-fn handle_read(library: Container<IOLibrary>, file: File, _sub_matches: &ArgMatches) -> ssize_t {
-    let file_len = file.metadata().expect("Failed to calculate file length").len() as usize;
-    let mut buf= Vec::<u8>::with_capacity(file_len as usize);
+fn handle_read(library: Container<IOLibrary>, mut file: File, offset: u64, count: Option<usize>) -> ssize_t {
+    let count = match count {
+        Some(count) => count,
+        None => file.metadata().expect("Failed to calculate file length").len() as usize,
+    };
+
+    if offset != 0
+    {
+        file.seek(SeekFrom::Start(offset as u64)).expect("Failed to set offset");
+    }
+    let mut buf= Vec::<u8>::with_capacity(count as usize);
     let buf_ptr = buf.as_mut_ptr();
     std::mem::forget(buf);
-    let result = library.read_all(file.as_raw_fd(), buf_ptr, file_len as size_t);
+    let result = library.read_all(file.as_raw_fd(), buf_ptr, count as size_t);
     
     let result_length = std::cmp::max(result, 0) as usize;
     let buffer = unsafe {
         Vec::from_raw_parts(
             buf_ptr,
             result_length,
-            file_len)
+            count)
     };
     println!("{}", String::from_utf8(buffer).expect("Failed to decode data"));
     result
 }
 
-fn handle_write(library: Container<IOLibrary>, file: File, sub_matches: &ArgMatches) -> ssize_t {
-    let data: String = sub_matches.value_of("data").expect("required").into();
+fn handle_write(library: Container<IOLibrary>, file: File, data: String) -> ssize_t {
     let data: CString = CString::new(data).unwrap();
     let bytes = data.as_bytes();
 
@@ -55,34 +98,26 @@ fn handle_write(library: Container<IOLibrary>, file: File, sub_matches: &ArgMatc
 fn main() {
     env_logger::init();
 
-    // todo: add a feature to seek the file before calling functions
-    let matches = Command::new("Tester")
-        .version("wip")
-        .author("AmirB")
-        .about("Tests a given r/w wrapper library")
-        .arg(arg!(<library_path> "The path to the library"))
-        .arg(arg!(<file_path> "The path to the file to read"))
-        .subcommand_required(true)
-        .subcommand(
-            Command::new("read")
-                .about("calls the read_all function of the library")
-        )
-        .subcommand(
-            Command::new("write")
-                .about("calls the write_all function of the libraray")
-                .arg(arg!(<data> "The data to write"))
-        )
-        .get_matches();
+    let cli = TesterCli::parse();
 
-    let library_path: String = matches.value_of("library_path").expect("required").into();
-    let file_path: String = matches.value_of("file_path").expect("required").into();
+
+    let library_path = &cli.library_path;
+    let file_path = &cli.file_path;
     
     let library = load_library(library_path);
 
-    let result = match matches.subcommand() {
-        Some(("read", sub_matches)) => handle_read(library, open_file(file_path, false), sub_matches),
-        Some(("write", sub_matches)) => handle_write(library, open_file(file_path, true), sub_matches),
-        _ => unreachable!()
+    let result = match cli.command {
+        Commands::Read { offset, count } => handle_read(
+            library,
+            open_file(file_path, false),
+            offset,
+            count
+        ),
+        Commands::Write { data } => handle_write(
+            library,
+            open_file(file_path, true),
+            data
+        ),
     };
 
     println!("{}", result);
